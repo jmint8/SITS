99dee3c6dd8e5ec384f049934db32bc5390ce3ee
jmint8 <jmitchelmeier@gmail.com>
2026-03-17 11:47:47 -0400

    roundRobin test written

--- src/test/java/SITS/RoundRobinTest.java
+++ src/test/java/SITS/RoundRobinTest.java
@@ -2,18 +2,49 @@ package SITS;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import SITS.Actions.AlwaysCooperate;
+import SITS.Actions.AlwaysDefect;
+import SITS.Actions.Participant;
+import SITS.Actions.TitForTat;
+import SITS.Game.Game;
+import SITS.Game.ItteratedPrisonersDilemma;
+import SITS.Game.RoundRobin;
+import SITS.Game.TournamentResult;
+
 class RoundRobinTest {
+	
+	RoundRobin tourna;
+	Game game;
+	
+	Participant alwaysCoop;
+    Participant alwaysDefect;
+    Participant tft;
+    List<Participant> players;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		tourna = new RoundRobin();
+		game = new ItteratedPrisonersDilemma(1);	
+		alwaysCoop = new AlwaysCooperate();
+        alwaysDefect = new AlwaysDefect();
+        tft = new TitForTat();
+        players = Arrays.asList(alwaysCoop, alwaysDefect, tft);
 	}
 
 	@Test
-	void test() {
-		//TDOD
+	void RoundRobinTest() {
+		TournamentResult result = tourna.run(players, game);
+		
+		assertEquals(5, result.getScores("Always Cooperate"));
+        assertEquals(20, result.getScores("Always Defect"));
+        assertEquals(5, result.getScores("Tit For Tat"));
+        
 	}
 
 }
