efb0e065585e8e6a1a1aae4f7e08b7cad6756920
jmint8 <jmitchelmeier@gmail.com>
2026-04-02 18:34:34 -0400

    added attripbutes to the remote participant and still working on the methods

--- src/main/java/SITS/Remote/Network/RemoteParticipant.java
+++ src/main/java/SITS/Remote/Network/RemoteParticipant.java
@@ -3,13 +3,52 @@ package SITS.Remote.Network;
 import SITS.Actions.Action;
 import SITS.Actions.Participant;
 import SITS.Game.GameHistory;
+import java.util.function.Function;
+import org.springframework.web.client.RestTemplate;
 
 public class RemoteParticipant implements Participant {
+	
+	private final String name;
+	private final String url;
+	private final Function<String, Action> actionFactory;
+	private final RestTemplate restTemp;
+	
+	/**
+	 * @param name
+	 * @param url
+	 * @param actionFactory
+	 * @param restTemp
+	 * this one is for testing
+	 */
+	RemoteParticipant(String name, String url, Function<String, Action> actionFactory, RestTemplate restTemp) {
+		super();
+		this.name = name;
+		this.url = url;
+		this.actionFactory = actionFactory;
+		this.restTemp = restTemp;
+	}
+	
+	
+
+	/**
+	 * @param name
+	 * @param url
+	 * @param actionFactory
+	 */
+	public RemoteParticipant(String name, String url, Function<String, Action> actionFactory) {
+		super();
+		this.name = name;
+		this.url = url;
+		this.actionFactory = actionFactory;
+		this.restTemp = new RestTemplate();
+	}
+
+
 
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 	@Override
