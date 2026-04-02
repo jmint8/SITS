8244ce97416ff50d3e85eee930b5437b73cc650e
jmint8 <jmitchelmeier@gmail.com>
2026-04-02 18:15:31 -0400

    added the string action class that I still question but it is part of the design. also added the Registration request class.

--- src/main/java/SITS/Remote/Network/StringAction.java
+++ src/main/java/SITS/Remote/Network/StringAction.java
@@ -3,12 +3,20 @@ package SITS.Remote.Network;
 import SITS.Actions.Action;
 
 public class StringAction implements Action {
-	//im still Skeptical on this
+	//im still skeptical on this but it is the design so...
+	
+	private final String label;
 
 	@Override
 	public String getLabel() {
 		// TODO Auto-generated method stub
-		return null;
+		return label;
 	}
+	
+	public StringAction(String label)
+	{
+		this.label = label;
+	}
+	
 
 }
--- src/main/java/SITS/Remote/Network/dto/RegistrationRequest.java
+++ src/main/java/SITS/Remote/Network/dto/RegistrationRequest.java
@@ -1,5 +1,17 @@
 package SITS.Remote.Network.dto;
 
 public class RegistrationRequest {
+	
+	public String name;
+	public String ip;
+	public int port;
+	
+	public RegistrationRequest(String name,String ip,int port) {
+		this.name = name;
+		this.ip = ip;
+		this.port = port;
+	}
+	
+	
 
 }
