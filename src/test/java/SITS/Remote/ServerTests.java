f61d072ab039c559e34aec580111f887a7153ff4
jmint8 <jmitchelmeier@gmail.com>
2026-04-20 23:43:30 -0400

    added the first bit of a tournament registry test into the server test so I can run checkgit and test all the files. I might have to test HUMAn Paricipant in the network file because the configuration I have is by package not by class

--- src/test/java/SITS/Remote/ServerTests.java
+++ src/test/java/SITS/Remote/ServerTests.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import SITS.Remote.Server.NetworkedTournament;
+import SITS.Remote.Server.TournamentRegistry;
+
 class ServerTests {
 
 	@BeforeEach
@@ -12,8 +15,15 @@ class ServerTests {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void TournamentResgistryTest() {
+		TournamentRegistry reg = new TournamentRegistry();
+		
+		NetworkedTournament tourna = reg.get("ipd-1");
+		assertNotNull(tourna);
+		assertEquals("Iterated Prisoner's Dilemma",tourna.getName());
+		
+		
+		
 	}
 
 }
