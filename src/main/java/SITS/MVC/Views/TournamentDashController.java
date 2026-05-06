9a123c6c062bf780867be99186a8f967c3497531
jmint8 <jmitchelmeier@gmail.com>
2026-05-06 08:58:26 -0400

    added the tournamentDashboard and controller files, I havent made the connection in scenebuilder beacause I can't open it on this laptop anymore

---
+++ src/main/java/SITS/MVC/Views/TournamentDashController.java
@@ -0,0 +1,31 @@
+package SITS.MVC.Views;
+
+import SITS.MVC.Models.ViewTransitionModelInterface;
+import javafx.fxml.FXML;
+
+public class TournamentDashController 
+{
+	ViewTransitionModelInterface transModel; 
+	
+	//more to add here
+	
+	
+	
+	
+	
+	
+	@FXML
+	void onWatchClick()
+	{
+		//sends you to the next page where you watch the tournament unfold. 
+	}
+	
+	
+	@FXML
+	void onRefreshClick() 
+	{
+		//for the refresh button, reprint the listview by wiping it and then printing it out again
+	}
+	
+	
+}
