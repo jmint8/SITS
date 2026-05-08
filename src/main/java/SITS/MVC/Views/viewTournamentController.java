b6e37eac16b72d3ff03c1906f3f63f0e75352305
jmint8 <jmitchelmeier@gmail.com>
2026-05-07 22:03:00 -0400

    i forgot the buttons and listview in the controller

--- src/main/java/SITS/MVC/Views/viewTournamentController.java
+++ src/main/java/SITS/MVC/Views/viewTournamentController.java
@@ -3,21 +3,32 @@ package SITS.MVC.Views;
 import SITS.MVC.Models.ViewTransitionModelInterface;
 import SITS.MVC.Models.viewerModel;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 public class viewTournamentController 
 {
 	ViewTransitionModelInterface transModel;
 	viewerModel model;
 	
+	@FXML
+	private Button backButton;
+	
+	@FXML
+	private ListView<String> moveListView;
+	
 	public void setModel(ViewTransitionModelInterface tModel,viewerModel newModel )
 	{
 		this.transModel = tModel;
 		this.model = newModel;
+		
+		//moveListView.setItems(model.getTournamentList().getMoveList());
 	}
 	
 	@FXML
 	void onBackClick()
 	{
+		//model.deRegister();
 		transModel.showTournamentDash();
 		System.out.println("going back to Dashboard");
 		
