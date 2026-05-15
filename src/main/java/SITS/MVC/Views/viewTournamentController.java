4ec505ec817e24406c09e4f1e160b6821dcc715e
jmint8 <jmitchelmeier@gmail.com>
2026-05-15 01:51:06 -0400

    adding things to the viewTournamentController

--- src/main/java/SITS/MVC/Views/viewTournamentController.java
+++ src/main/java/SITS/MVC/Views/viewTournamentController.java
@@ -1,10 +1,15 @@
 package SITS.MVC.Views;
 
+
 import SITS.MVC.Models.ViewTransitionModelInterface;
 import SITS.MVC.Models.viewerModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import SITS.MVC.Models.EmojiNoteStrategy;
+import SITS.MVC.Models.NoteStrategy;
+import SITS.MVC.Models.NoteStrategyInterface;
 
 public class viewTournamentController 
 {
@@ -33,5 +38,39 @@ public class viewTournamentController
 		System.out.println("going back to Dashboard");
 		
 	}
+	
+	
+	//sprint 4 content again 
+	
+	@FXML
+	private ListView<String> notesListView;
+	
+	@FXML
+	private TextField noteInput;
+	
+	@FXML
+	void onPostClick()
+	{
+		
+	}
+	
+	@FXML
+	void onSmileyClick()
+	{
+		
+	}
+	
+	@FXML
+	void onNervyClick()
+	{
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
