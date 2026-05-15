6ceb38249fd8c02cb41cf893183be72f340896fc
jmint8 <jmitchelmeier@gmail.com>
2026-05-14 23:06:26 -0400

    added the viewTournament test for the buttons. we will test the list view later

---
+++ src/test/java/SITS/MVC/viewTournamentTest.java
@@ -0,0 +1,83 @@
+package SITS.MVC;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import SITS.MVC.Models.ViewTransitionModelInterface;
+import SITS.MVC.Models.viewerModel;
+import SITS.MVC.Views.connectionController;
+import SITS.MVC.main.Main;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+@ExtendWith(ApplicationExtension.class)
+public class viewTournamentTest implements ViewTransitionModelInterface {
+
+	int dashCalled;
+	viewerModel model;
+	
+	@Start
+	public void start(Stage stage)
+	{
+		dashCalled = 0;
+		model = new viewerModel();
+		
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("/SITS/MVC/Views/viewTournament.fxml"));
+		Pane view;
+		try {
+			
+			view = loader.load();
+			connectionController cont = loader.getController();
+			cont.setModel(this, model);
+			
+			Scene s = new Scene(view);
+			stage.setScene(s);
+			stage.show();	
+		} catch (IOException e) {e.printStackTrace();}
+		
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+	@Override
+	public void showConnection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showTournamentDash() {
+		dashCalled++;
+		
+	}
+
+	@Override
+	public void showViewTournament() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Test
+	public void testBackButtonToDash(FxRobot r)
+	{
+		r.clickOn("#backButton");
+		Assertions.assertThat(dashCalled).isEqualTo(1);
+		
+		
+	}
+
+}
